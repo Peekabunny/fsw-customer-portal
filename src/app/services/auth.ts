79b5ec80b458b9844fce6962eb33c32c227325e1
Vincent Do <vinvdo123@gmail.com>
2026-07-06 04:47:57 -0400

    Fix API URLs to point to Azure

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class AuthService {
 
-  private apiUrl = 'https://localhost:7227';
+  private apiUrl = 'https://fsw-support-api-eybrdfemfvdqfnek.westus3-01.azurewebsites.net';
 
   // injects an instance of HTTpClient 
   constructor(private http: HttpClient) {}
